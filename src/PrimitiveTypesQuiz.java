7b958ee17720e720362520f2320a5f31cd9e2523
Zehra Ozer <zozer4602@gmail.com>
2025-10-25 13:50:05 +0200

    First commit - Hello Vistula Added PrimitiveTypesQuiz task

---
+++ src/PrimitiveTypesQuiz.java
@@ -0,0 +1,15 @@
+public class PrimitiveTypesQuiz {
+    public static void main(String[] args) {
+        int age = 25;
+        float weight = 55.6f;
+        double height = 1.70;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is student? " + isStudent);
+    }
+}
